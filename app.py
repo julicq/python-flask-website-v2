093a3b89ab8a64ac1a195d414ca28b391d609206
julianPavlov <62881010+julicq@users.noreply.github.com>
2023-02-22 09:46:33 +0000

    colors to machines

--- app.py
+++ app.py
@@ -5,31 +5,31 @@ app = Flask(__name__)
 BRANDS = [{
   'id': 1,
   'title': 'Biesse Wood',
-  'color': 'orange'
+  'machines': 'Rover, Selco, Opera, Skipper, Akron'
 }, {
   'id': 2,
   'title': 'Biesse Glass',
-  'color': 'blue'
+  'machines': 'Master, Vertmax, Busetti, Genius'
 }, {
   'id': 3,
   'title': 'Biesse Stone',
-  'color': 'red'
+  'machines': 'Smart, Master, Movetro, Luna'
 }, {
   'id': 4,
   'title': 'Biesse Materia',
-  'color': 'violet'
+  'machines': 'Rover Plast, Terma, Materia'
 }]
 
 
 @app.route("/")
 def hello_world():
-  return render_template('home.html', 
-                         jobs=BRANDS, 
-                         company_name="Biesse")
+  return render_template('home.html', jobs=BRANDS, company_name="Biesse")
+
 
 @app.route("/api/brands")
 def list_brands():
   return jsonify(BRANDS)
+
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
